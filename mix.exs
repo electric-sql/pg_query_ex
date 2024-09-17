ffcb7345fd2b1fe908f37b2b0d5806cca2b2065d
Garry Hill <garry@electric-sql.com>
2024-09-17 11:16:21 +0100

    Add hex.pm package info (#4)

--- mix.exs
+++ mix.exs
@@ -4,11 +4,17 @@ defmodule PgQuery.MixProject do
   def project do
     [
       app: :pg_query_ex,
-      version: "0.1.0",
       elixir: "~> 1.13",
+      version: "0.5.4",
       start_permanent: Mix.env() == :prod,
       compilers: [:elixir_make] ++ Mix.compilers(),
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      name: "PgQuery",
+      package: package(),
+      docs: docs(),
+      source_url: "https://github.com/electric-sql/pg_query_ex",
+      homepage_url: "https://electric-sql.com"
     ]
   end
 
@@ -23,7 +29,45 @@ defmodule PgQuery.MixProject do
   defp deps do
     [
       {:elixir_make, "~> 0.4", runtime: false},
-      {:protox, "~> 1.7"}
+      {:protox, "~> 1.7"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    # Exclude the test files and other bits and bobs to sneak us in under the
+    # 8MB hex.pm package size limit...
+    c_src = ~w(
+      c_src/libpg_query_ex.c
+      c_src/libpg_query/LICENSE
+      c_src/libpg_query/Makefile
+      c_src/libpg_query/README.md
+      c_src/libpg_query/pg_query.h
+      c_src/libpg_query/patches
+      c_src/libpg_query/protobuf
+      c_src/libpg_query/src
+      c_src/libpg_query/vendor
+    )
+
+    [
+      files: ~w(lib .formatter.exs mix.exs README.* LICENSE Makefile) ++ c_src,
+      licenses: ["Apache-2.0"],
+      links: %{
+        "libpg_query" => "https://github.com/pganalyze/libpg_query",
+        "Github" => "https://github.com/electric-sql/pg_query_ex",
+        "Electric SQL" => "https://electric-sql.com"
+      }
+    ]
+  end
+
+  defp description do
+    "An Elixir interface to the libpg_query postgres query parser. Converts a valid PostgreSQL query into a Protobuf based AST."
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md", "LICENSE"]
     ]
   end
 end
