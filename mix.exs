f62000286c5f5f6508fe867f8393c52418c47b18
Oleksii Sholik <oleksii@sholik.dev>
2025-06-30 14:10:48 +0200

    Bump version to 0.8.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PgQuery.MixProject do
     [
       app: :pg_query_ex,
       elixir: "~> 1.13",
-      version: "0.7.0",
+      version: "0.8.0",
       start_permanent: Mix.env() == :prod,
       compilers: [:elixir_make] ++ Mix.compilers(),
       deps: deps(),
